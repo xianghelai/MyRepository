a12749359036f36484f8a1e37fb3ba25a98d3f7f
lxh <1059653473@qq.com>
2018-06-11 17:09:48 +0800

    项目1

---
+++ 02_maven/src/test/java/cn/itcast/util/test/LoadPropertyTest.java
@@ -0,0 +1,15 @@
+package cn.itcast.util.test;
+
+import cn.itcast.util.loadPro;
+
+public class LoadPropertyTest {
+	/*public static void main(String[] args) {
+		loadPro loadPro = new loadPro();
+		String str = loadPro.getProperties("msg");
+		System.out.println(str);
+	}*/
+	@test
+	public void test01() {
+		
+	}
+}
