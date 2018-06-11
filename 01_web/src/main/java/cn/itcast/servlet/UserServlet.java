d8ad626696d010adee0386efdd4ea0b121f27ea5
lxh <1059653473@qq.com>
2018-06-11 16:43:49 +0800

    1

---
+++ 01_web/src/main/java/cn/itcast/servlet/UserServlet.java
@@ -0,0 +1,29 @@
+package cn.itcast.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+		resp.setCharacterEncoding("utf-8");
+		PrintWriter writer = resp.getWriter();
+		writer.println("合肥人居多");
+		
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doGet(req, resp);
+	}
+}
